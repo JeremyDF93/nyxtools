7ddfa4c2ece88b87d9a3c2e2950f21f7c8233d25
Jeremy Farmer <jeremydf93@hotmail.com>
2019-12-29 18:44:31 -0800

    [spcomp] treat warnings as errors

--- scripting/compile.sh
+++ scripting/compile.sh
@@ -1,11 +1,13 @@
 #!/bin/bash -e
+cd "$(dirname "$0")"
+
 SMSDK=../../alliedmodders/sourcemod
 SPCOMP=spcomp
 
 ###
 ### You shouldn't need to edit below this line.
 ###
-build() {
+git_count() {
   git rev-list --all --count
 }
 
@@ -13,10 +15,8 @@ VERSION=`cat VERSION`
 MAJOR=`echo $VERSION | cut -d. -f1`
 MINOR=`echo $VERSION | cut -d. -f2`
 REVISION=`echo $VERSION | cut -d. -f3`
-BUILD_STRING="git$(build)"
+BUILD_STRING="git$(git_count)"
 eval "echo \"$(cat include/nyxtools_version.tpl)\"" > include/nyxtools_version.inc
-
-cd "$(dirname "$0")"
 
 test -e compiled || mkdir compiled
 
@@ -25,13 +25,13 @@ if [[ $# -ne 0 ]]; then
   do
     smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
     echo -e "\nCompiling $sourcefile..."
-    $SPCOMP $sourcefile -iinclude -i$SMSDK/plugins/include -ocompiled/$smxfile
+    $SPCOMP $sourcefile -iinclude -i$SMSDK/plugins/include -ocompiled/$smxfile -E
   done
 else
   for sourcefile in *.sp
   do
     smxfile="`echo $sourcefile | sed -e 's/\.sp$/\.smx/'`"
     echo -e "\nCompiling $sourcefile ..."
-    $SPCOMP $sourcefile -iinclude -i$SMSDK/plugins/include -ocompiled/$smxfile
+    $SPCOMP $sourcefile -iinclude -i$SMSDK/plugins/include -ocompiled/$smxfile -E
   done
 fi
