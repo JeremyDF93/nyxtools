ef536b9a3a0944d0f2b84f7f5783fa66de2ea974
Jeremy Farmer <jeremydf93@hotmail.com>
2019-12-29 17:59:04 -0800

    add versioning template

--- scripting/compile.sh
+++ scripting/compile.sh
@@ -2,6 +2,20 @@
 SMSDK=../../alliedmodders/sourcemod
 SPCOMP=spcomp
 
+###
+### You shouldn't need to edit below this line.
+###
+build() {
+  git rev-list --all --count
+}
+
+VERSION=`cat VERSION`
+MAJOR=`echo $VERSION | cut -d. -f1`
+MINOR=`echo $VERSION | cut -d. -f2`
+REVISION=`echo $VERSION | cut -d. -f3`
+BUILD_STRING="git$(build)"
+eval "echo \"$(cat include/nyxtools_version.tpl)\"" > include/nyxtools_version.inc
+
 cd "$(dirname "$0")"
 
 test -e compiled || mkdir compiled
---
+++ scripting/include/nyxtools_version.tpl
@@ -0,0 +1,6 @@
+#if defined _nyx_version_included
+  #endinput
+#endif
+#define _nyx_version_included
+
+#define NYX_PLUGIN_VERSION \"${MAJOR}.${MINOR}.${REVISION}-${BUILD_STRING}\"
