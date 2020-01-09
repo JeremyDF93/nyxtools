19160c42d2d80e339da38c69095519895b8e9fa1
Jeremy Farmer <jeremydf93@hotmail.com>
2020-01-08 18:32:51 -0800

    Update configure.py

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from ambuild2 import run
 builder = run.PrepareBuild(sourcePath = sys.path[0])
 
 builder.options.add_option('--hl2sdk-root', type=str, dest='hl2sdk_root', default=None,
-		                   help='Root search folder for HL2SDKs')
+                       help='Root search folder for HL2SDKs')
 builder.options.add_option('--mms-path', type=str, dest='mms_path', default=None,
                        help='Path to Metamod:Source')
 builder.options.add_option('--sm-path', type=str, dest='sm_path', default=None,
